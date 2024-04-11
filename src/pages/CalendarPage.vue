ab7b1cf2620ca63d1c30af3f4b21104ae7f3c996
Gabriel Areco <areco23@gmail.com>
2024-04-11 09:59:58 -0400

    FEAT:(Dev Calendar): First implementation of draggable calendar events

---
+++ src/pages/CalendarPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <CalendarComponent />
+  </q-page>
+</template>
+<script setup>
+import CalendarComponent from 'components/CalendarComponent.vue';
+</script>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <LoginComponent />
+  </q-page>
+</template>
+
+<script setup>
+import LoginComponent from 'components/LoginComponent.vue';
+</script>
